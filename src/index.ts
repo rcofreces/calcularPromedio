bd04e651667af8792bb7db589250f8a4753362f5
rcofreces <104039731+rcofreces@users.noreply.github.com>
2022-06-01 20:22:03 -0300

    commit01/06 No sé cómo sacar el promedio tdv. Queda esa parte porque no puedo ir sumando las edades aleatorias en el for.

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,21 @@
-let dimensionCantidad: number = Number(prompt("Ingrese cuantos chicos hay en el equipo de fútbol"));
-let cantidadChicos: number[] = new Array (dimensionCantidad);
-let edadChicos: number[] = [3, 4, 5, 6, 7];
-  
-function aleatorio(menorValor: number, mayorValor: number, indice: number) {
-  let resultado: number;
-  for (indice = 0; indice < cantidadChicos.length; indice++) {
-    resultado = Math.random() * (7 - 3) + 3;
-    indice++;
-    console.log("Edad del alumno es", resultado.toFixed());
+let dimensionCantidad: number = Number(
+  prompt("Ingrese cuantos chicos hay en el equipo de fútbol")
+);
+let cantidadChicos: number[] = new Array(dimensionCantidad);
+let edadesChicos: number[] = new Array(dimensionCantidad);
+let promedio: number = 0;
+
+function aleatorio(menorValor: number, mayorValor: number): void {
+  let resultado: number = 0;
+  menorValor = 3;
+  mayorValor = 7;
+  for (let indice: number = 0; indice < cantidadChicos.length; indice++) {
+    resultado = Math.random() * (mayorValor - menorValor) + menorValor;
+    console.log(`Edad del alumno ${edadesChicos[indice] + 1} es ${resultado.toFixed()}`);
   }
 }
 
-aleatorio(3, 7, 0);
+aleatorio(3, 7)
 
-
-/*function dibujarGuionesv2(cantidad: number) {
-  let linea: string = "-";
-  for (let x: number = 1; x <= cantidad; x++) {
-    linea += "-"; // linea = linea + "-"
-  }
-  console.log(linea);
-}
-
-console.log("Dibuja guiones recibiendo la cantidad de lineas por parametro");
-dibujarGuionesv2(Number(prompt("Ingrese la cantidad de guiones")));
-dibujarGuionesv2(45);*/
-
+//promedio = aleatorio(3, 7) / 5;
+//console.log(`El promedio de edades de los chicos de fútbol es ${promedio}`);
